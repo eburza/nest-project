fdd8f17c0cb3c36c720beb670252d6b3bfdaa238
Emilia Burza <emila.burza@gmail.com>
2025-03-13 18:37:08 +0100

    Inject users provider into users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,39 +8,50 @@ import {
   Body,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  /*
-  GET /users
-  GET /users/:id
-  POST /users
-  PATCH /users/:id (update) some can use PUT
-  DELETE /users/:id
-  */
+  // inject methods from provider
+  constructor(private readonly usersService: UsersService) {}
 
   @Get() // GET /users or /users?role=value => query params
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [{ role }];
+    return this.usersService.findAll(role); // call method
   }
 
   @Get(':id') // GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id); // unary plus (+) operator convert operand into a number, if it isn't already
   }
 
   @Post() // POST /user
-  create(@Body() user: object) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: object) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
   remove(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(+id);
   }
 }
