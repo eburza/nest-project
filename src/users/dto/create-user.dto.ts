74e162a8691f01b867d3ade8abbab1f5214ca067
Emilia Burza <emila.burza@gmail.com>
2025-03-13 21:30:24 +0100

    Add validator to user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,25 @@
+// disable no-unsafe-call for decorators because they are not supported by class-validator
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export enum UserRole {
+  INTERN = 'INTERN',
+  ENGINEER = 'ENGINEER',
+  ADMIN = 'ADMIN',
+}
 // create user data transfer object
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
-  role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+
+  @IsEnum(UserRole, {
+    message: 'Role must be either INTERN, ENGINEER or ADMIN', // custom error message
+  })
+  @IsNotEmpty()
+  role: UserRole;
 }
