9e0c99528d13ff0bf287c48cfbb351fe42e0ba5d
Emilia Burza <emila.burza@gmail.com>
2025-03-14 22:05:55 +0100

    Add CORS to the app

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // enable cors to allow the api to be accessed from the frontend
+  // this will allow the api to be accesssed by any origin
+  app.enableCors();
+  // set the global prefix for the api
+  app.setGlobalPrefix('api');
+  // use global pipes to validate the data
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
