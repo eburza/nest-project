b53e3286024b3095a701109d724f19b29edb15da
Emilia Burza <emila.burza@gmail.com>
2025-03-14 23:18:59 +0100

    Add logger to employees controller

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,27 +7,37 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { MyLoggerService } from 'src/my-logger/my-logger.service';
 import { UserRole } from 'src/users/dto/create-user.dto';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  // create a logger for the controller
+  private readonly logger = new MyLoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
+    // log the creation of an employee
+    this.logger.log('Creating employee');
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get()
-  findAll(@Query('role') role?: UserRole) {
+  findAll(@Ip() ip: string, @Query('role') role?: UserRole) {
+    // log the finding of all employees with the ip address
+    this.logger.log(`Request for all employees ${ip}`);
     return this.employeesService.findAll(role);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    // log the finding of an employee by id
+    this.logger.log('Finding employee by id');
     return this.employeesService.findOne(+id);
   }
 
@@ -36,11 +46,15 @@ export class EmployeesController {
     @Param('id') id: string,
     @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
   ) {
+    // log the updating of an employee
+    this.logger.log('Updating employee');
     return this.employeesService.update(+id, updateEmployeeDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
+    // log the removal of an employee
+    this.logger.log('Removing employee');
     return this.employeesService.remove(+id);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,9 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bufferLogs: true, // buffer the logs to avoid any issues with the logger
-  });
-  // use the my logger service to log the messages
-  app.useLogger(app.get(MyLoggerService));
+  const app = await NestFactory.create(AppModule);
   // enable cors to allow the api to be accessed from the frontend
   // this will allow the api to be accesssed by any origin
   app.enableCors();
