4855585d41c147fa42123bfdcf901108729cbabe
Emilia Burza <emila.burza@gmail.com>
2025-03-14 22:15:11 +0100

    Add throtller to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, EmployeesModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    EmployeesModule,
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]), // throttle requests to 10 requests per minute
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD, // provide the throttler guard to the app
+      useClass: ThrottlerGuard, // use the throttler guard in the app
+    },
+  ],
 })
 export class AppModule {}
